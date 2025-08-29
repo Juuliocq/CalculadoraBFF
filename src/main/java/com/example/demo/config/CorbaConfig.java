cb2d8d24a79b4d155b0cfde1bfa81a770a4b56dc
Juuliocq <julio.cq96@gmail.com>
2025-08-29 11:39:38 -0300

    ajuste

--- src/main/java/com/example/demo/config/CorbaConfig.java
+++ src/main/java/com/example/demo/config/CorbaConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import org.omg.CORBA.ORB;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,10 +10,13 @@ import java.util.Properties;
 @Configuration
 public class CorbaConfig {
 
+    @Value("${corba.service.url}")
+    private String corbaServiceUrl;
+
     @Bean
     public ORB orb() {
         Properties props = new Properties();
-        props.setProperty("ORBInitRef.NameService", "corbaloc::localhost:1050/NameService");
+        props.setProperty("ORBInitRef.NameService", corbaServiceUrl);
 
         return org.jacorb.orb.ORB.init(new String[0], props);
     }
--- src/main/java/com/example/demo/controller/CalculatorController.java
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -2,11 +2,13 @@ package com.example.demo.controller;
 
 import com.example.demo.service.CalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*") // Permite de qualquer origem
 public class CalculatorController {
 
     @Autowired
